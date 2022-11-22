e6544d79f8e73433b83bc98dbe4b1f142e8307c7
Lukas Andersson <lukas.s.andersson@gmail.com>
2022-11-22 20:36:51 +0100

    Updated structure of data

--- index.js
+++ index.js
@@ -8,86 +8,94 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/search", (req, res) => {
-  //console.log(`Handling: ${req.headers.url}`);
-  if (req.headers?.url) {
-    axios
-      .get(req.headers.url)
-      .then((result) => {
-        codes = [];
-        courses = [];
+	//console.log(`Handling: ${req.headers.url}`);
+	if (req.headers?.url) {
+		axios
+			.get(req.headers.url)
+			.then((result) => {
+				codes = [];
+				courses = [];
 
-        data = result.data
-          .split("<!-- Result Navigation Pane END -->")[1]
-          .split("<!-- Result Navigation Pane START -->")[0]
-          .split("</tr>")
-          .slice(2);
+				data = result.data
+					.split("<!-- Result Navigation Pane END -->")[1]
+					.split("<!-- Result Navigation Pane START -->")[0]
+					.split("</tr>")
+					.slice(2);
 
-        data.pop();
+				data.pop();
 
-        data.forEach((ele) => {
-          split = ele.split("<td").slice(1);
+				data.forEach((ele) => {
+					split = ele.split("<td").slice(1);
 
-          courseCode = split[0].slice(
-            split[0].slice(0, split[0].search("</a>")).lastIndexOf(">") + 1,
-            split[0].search("</a>")
-          );
-          courseName = split[1].slice(
-            split[1].slice(0, split[1].search("</a>")).lastIndexOf(">") + 1,
-            split[1].search("</a>")
-          );
+					courseCode = split[0].slice(
+						split[0]
+							.slice(0, split[0].search("</a>"))
+							.lastIndexOf(">") + 1,
+						split[0].search("</a>")
+					);
+					courseName = split[1].slice(
+						split[1]
+							.slice(0, split[1].search("</a>"))
+							.lastIndexOf(">") + 1,
+						split[1].search("</a>")
+					);
 
-          points = split[2].slice(1, split[2].search("&"));
+					points = split[2].slice(1, split[2].search("&"));
 
-          department = split[3].slice(1, split[3].search("<"));
+					department = split[3].slice(1, split[3].search("<"));
 
-          courseInfo =
-            "https://student.portal.chalmers.se" +
-            split[4].slice(
-              split[4].search('"') + 1,
-              split[4].slice(split[4].search('"') + 1).search('"') +
-                split[4].search('"') +
-                1
-            );
+					courseInfo =
+						"https://student.portal.chalmers.se" +
+						split[4].slice(
+							split[4].search('"') + 1,
+							split[4]
+								.slice(split[4].search('"') + 1)
+								.search('"') +
+								split[4].search('"') +
+								1
+						);
 
-          courseHome = split[5].slice(
-            split[5].search("https"),
-            split[5].slice(split[5].search("https")).search('"') +
-              split[5].search("https")
-          );
+					courseHome = split[5].slice(
+						split[5].search("https"),
+						split[5].slice(split[5].search("https")).search('"') +
+							split[5].search("https")
+					);
 
-          codes.push(`https://stats.ftek.se/courses/${courseCode}`);
+					codes.push(`https://stats.ftek.se/courses/${courseCode}`);
 
-          courses.push([
-            courseCode,
-            courseName,
-            points,
-            department,
-            courseInfo,
-            courseHome,
-          ]);
-        });
+					courses.push({
+						Code: courseCode,
+						Name: courseName,
+						Credit: points,
+						Institution: department,
+						CourseInfo: courseInfo,
+						Canvas: courseHome,
+					});
+				});
 
-        axios.all(codes.map((code) => axios.get(code))).then(
-          axios.spread((...resData) => {
-            res.json({
-              Courses: courses.map((course, index) => {
-                if (resData[index].data) {
-                  passInfo = [
-                    resData[index].data.passRate,
-                    resData[index].data.averageGrade,
-                    resData[index].data.totalPass,
-                  ];
-                  return course.concat(passInfo);
-                } else {
-                  return course;
-                }
-              }),
-            });
-          })
-        );
-      })
-      .catch((error) => console.log(error));
-  }
+				axios.all(codes.map((code) => axios.get(code))).then(
+					axios.spread((...resData) => {
+						res.json({
+							Courses: courses.map((course, index) => {
+								if (resData[index].data) {
+									return {
+										...course,
+										PassRate: resData[index].data.passRate,
+										AverageGrade:
+											resData[index].data.averageGrade,
+										SampleSize:
+											resData[index].data.totalPass,
+									};
+								} else {
+									return course;
+								}
+							}),
+						});
+					})
+				);
+			})
+			.catch((error) => console.log(error));
+	}
 });
 
 app.listen(8080, () => console.log("API Server is running..."));
